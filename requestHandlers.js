ec9c4b62bba6c8a593dfca7cc8920483caeef6eb
TonyHui <thlcly@163.com>
2016-07-28 17:20:04 +0800

    实现post请求的文本上传以及回显(要注意post中的中文乱码的处理)

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,22 +4,42 @@
 
 // 在node中异步执行命令行命令的lib
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+  /*
   // 异步执行, 可以让本次请求快速结束从而让node去接受下一次的请求(如果在请求中有阻塞的方法, 那后面请求也会阻塞, 因为node是单线程的, 基于事件轮询的)
   exec("find /", {timeout:100000, maxBuffer: 20000*1024}, function(err, stdout, stderr){
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write(stdout);
         response.end();
   });
+  */
 }
 
-function upload(response) {
-    console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
-    response.end();
+function upload(response, postData) {
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+  response.write("You've sent:" + querystring.parse(postData).text);
+  response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -3,9 +3,27 @@ var url = require("url");
 
 function start(route, handle) {
     http.createServer(function(request, response){
+
+        var postData = "";
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        request.setEncoding("utf-8");
+
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
+        /**
+         * 路由 
         var pathname = url.parse(request.url).pathname;
         console.log("request for" + pathname + " received.");
         route(handle, pathname, response);
+        */
     }).listen(8888);
     console.log("Server has started");
 }
