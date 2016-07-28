8bb9956c431100db304057d52c74cb7c0f67a248
TonyHui <thlcly@163.com>
2016-07-28 16:51:43 +0800

    将请求处理函数中耗时的操作修改成异步调用

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,14 +2,24 @@
  * 请求处理文件
  */
 
-function start() {
+// 在node中异步执行命令行命令的lib
+var exec = require("child_process").exec;
+
+function start(response) {
   console.log("Request handler 'start' was called.");
-  return "Hello Start";
+  // 异步执行, 可以让本次请求快速结束从而让node去接受下一次的请求(如果在请求中有阻塞的方法, 那后面请求也会阻塞, 因为node是单线程的, 基于事件轮询的)
+  exec("find /", {timeout:100000, maxBuffer: 20000*1024}, function(err, stdout, stderr){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+  });
 }
 
-function upload() {
-  console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+function upload(response) {
+    console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -5,10 +5,7 @@ function start(route, handle) {
     http.createServer(function(request, response){
         var pathname = url.parse(request.url).pathname;
         console.log("request for" + pathname + " received.");
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        var content = route(handle, pathname);
-        response.write(content);
-        response.end();
+        route(handle, pathname, response);
     }).listen(8888);
     console.log("Server has started");
 }
