c072d1b40aed65a4de9533edffa983842c074c92
TonyHui <thlcly@163.com>
2016-07-28 14:19:16 +0800

    http服务器添加url解析功能

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var http = require("http");
+var url = require("url");
 
 function start() {
     http.createServer(function(request, response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("request for" + pathname + " received.");
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("hello world");
         response.end();
     }).listen(8888);
+    console.log("Server has started");
 }
 
 exports.start = start;
